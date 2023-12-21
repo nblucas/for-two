dbe7fd0ce223233bd5f88c7f753a7ffda1a49d02
nblucas <lucasnunesbossle@gmail.com>
2023-12-21 15:28:39 -0300

    One more section added

--- index.js
+++ index.js
@@ -17,15 +17,19 @@ function nextText() {
     else if(id === "text-2") {
         changeDisplays("text-2", "text-3")
         id = "text-3"  
-        
+    }
+    else if(id === "text-3") {
+        changeDisplays("text-3", "text-4")
+        id = "text-4"
+
         buttonGeneric = document.getElementById("button-generic")
         buttonReturn = document.getElementById("button-return")
 
         buttonGeneric.style.display = "none"
         buttonReturn.style.display = "inline-block"
     }
-    else if(id === "text-3") {
-        changeDisplays("text-3", "text-1")
+    else if(id === "text-4") {
+        changeDisplays("text-4", "text-1")
         id = "text-1"
 
         buttonGeneric = document.getElementById("button-generic")
