14c0981412c4fda5f98e2db0aa5edd3d7e24d222
nblucas <lucasnunesbossle@gmail.com>
2023-12-21 15:13:13 -0300

    Multiple sections

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let id = "text-1"
+
+function changeDisplays(id1, id2) {
+    prev = document.getElementById(id1)
+    next = document.getElementById(id2)
+
+    prev.style.display = "none"
+    next.style.display = "flex"
+}
+
+function nextText() {
+    if(id === "text-1") {
+        changeDisplays("text-1", "text-2")
+        id = "text-2"       
+    }
+    else if(id === "text-2") {
+        changeDisplays("text-2", "text-3")
+        id = "text-3"  
+        
+        buttonGeneric = document.getElementById("button-generic")
+        buttonReturn = document.getElementById("button-return")
+
+        buttonGeneric.style.display = "none"
+        buttonReturn.style.display = "inline-block"
+    }
+    else if(id === "text-3") {
+        changeDisplays("text-3", "text-1")
+        id = "text-1"
+
+        buttonGeneric = document.getElementById("button-generic")
+        buttonReturn = document.getElementById("button-return")
+
+        buttonGeneric.style.display = "inline-block"
+        buttonReturn.style.display = "none"
+    }
+}
